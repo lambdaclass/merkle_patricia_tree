a8f23516657ae1dd6e0111efa8260e8d77570b8e
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-01-02 18:19:32 +0100

    Implement benchmarking for `PatriciaTree::remove` both with existing and nonexistent nodes.

--- benches/patricia_tree.rs
+++ benches/patricia_tree.rs
@@ -1,7 +1,7 @@
-use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Bencher, Criterion};
 use patricia_tree::PatriciaTree;
 use rand::{distributions::Standard, prelude::*};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.benchmark_group("PatriciaTree::<i32>::get()")
@@ -21,6 +21,18 @@ fn criterion_benchmark(c: &mut Criterion) {
         .bench_function("10k", bench_insert_override::<i32, 10_000>)
         .bench_function("100k", bench_insert_override::<i32, 100_000>)
         .bench_function("1M", bench_insert_override::<i32, 1_000_000>);
+
+    c.benchmark_group("PatriciaTree::<i32>::remove()")
+        .bench_function("1k", bench_remove::<i32, 1_000>)
+        .bench_function("10k", bench_remove::<i32, 10_000>)
+        .bench_function("100k", bench_remove::<i32, 100_000>)
+        .bench_function("1M", bench_remove::<i32, 1_000_000>);
+
+    c.benchmark_group("PatriciaTree::<i32>::remove() nonexistent")
+        .bench_function("1k", bench_remove_nonexistent::<i32, 1_000>)
+        .bench_function("10k", bench_remove_nonexistent::<i32, 10_000>)
+        .bench_function("100k", bench_remove_nonexistent::<i32, 100_000>)
+        .bench_function("1M", bench_remove_nonexistent::<i32, 1_000_000>);
 }
 
 criterion_group!(benches, criterion_benchmark);
@@ -87,7 +99,7 @@ where
             let (key, value) = key_iter.next().unwrap();
             black_box(tree.insert(key, *value));
         },
-        criterion::BatchSize::SmallInput,
+        BatchSize::SmallInput,
     )
 }
 
@@ -115,4 +127,70 @@ where
         let (key, value) = key_iter.next().unwrap();
         black_box(tree.insert(key, *value));
     });
+}
+
+fn bench_remove<T, const N: usize>(b: &mut Bencher)
+where
+    T: Copy,
+    Standard: Distribution<T>,
+{
+    // Generate a completely random Patricia tree.
+    let mut tree = PatriciaTree::<T>::new();
+    let mut all_keys = Vec::with_capacity(N);
+
+    while all_keys.len() < N {
+        let key = random::<[u8; 32]>();
+        let value = random::<T>();
+
+        if tree.insert(&key, value).is_none() {
+            all_keys.push(key);
+        }
+    }
+
+    // Run the benchmark.
+    let mut key_iter = all_keys.iter().cycle();
+    b.iter_batched(
+        || tree.clone(),
+        |mut tree| black_box(tree.remove(key_iter.next().unwrap())),
+        BatchSize::SmallInput,
+    )
+}
+
+fn bench_remove_nonexistent<T, const N: usize>(b: &mut Bencher)
+where
+    T: Copy,
+    Standard: Distribution<T>,
+{
+    // Generate a completely random Patricia tree.
+    let mut tree = PatriciaTree::<T>::new();
+    let mut all_keys = Vec::with_capacity(N);
+
+    while all_keys.len() < N {
+        let key = random::<[u8; 32]>();
+        let value = random::<T>();
+
+        if tree.insert(&key, value).is_none() {
+            all_keys.push(key);
+        }
+    }
+
+    // Generate random nonexisting keys (for removal).
+    let mut insert_data = HashSet::new();
+    while insert_data.len() < 1024 {
+        let key = random::<[u8; 32]>();
+
+        if tree.get(&key).is_none() {
+            insert_data.insert(key);
+        }
+    }
+
+    let insert_data = insert_data.into_iter().collect::<Vec<_>>();
+
+    // Run the benchmark.
+    let mut key_iter = insert_data.iter().cycle();
+    b.iter_batched(
+        || tree.clone(),
+        |mut tree| black_box(tree.remove(key_iter.next().unwrap())),
+        BatchSize::SmallInput,
+    )
 }
