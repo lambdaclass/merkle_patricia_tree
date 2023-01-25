50c2ed0fcba50ef1783cf8335c1ed0cb2565995d
Edgar <git@edgarluque.com>
2023-01-25 12:50:07 +0100

    easier profiling program

--- Makefile
+++ Makefile
@@ -19,7 +19,12 @@ bench:
 	cargo bench
 
 profile:
-	 cargo build --examples --profile=release-with-debug && valgrind --tool=dhat ./target/release-with-debug/examples/calculate-root
+	 cargo build --examples --profile=release-with-debug && \
+	 	valgrind --tool=dhat --dhat-out-file=dhat.out.n-100 ./target/release-with-debug/examples/calculate-root 100 && \
+		valgrind --tool=dhat --dhat-out-file=dhat.out.n-1000 ./target/release-with-debug/examples/calculate-root 1000 && \
+		valgrind --tool=dhat --dhat-out-file=dhat.out.n-10000 ./target/release-with-debug/examples/calculate-root 10000 && \
+		valgrind --tool=dhat --dhat-out-file=dhat.out.n-100000 ./target/release-with-debug/examples/calculate-root 100000 && \
+		valgrind --tool=dhat --dhat-out-file=dhat.out.n-500000 ./target/release-with-debug/examples/calculate-root 500000
 
 coverage:
 	cargo tarpaulin
--- examples/calculate-root.rs
+++ examples/calculate-root.rs
@@ -1,17 +1,27 @@
+use std::error::Error;
+
 use patricia_merkle_tree::PatriciaMerkleTree;
+use rand::{thread_rng, RngCore};
 use sha3::Keccak256;
 
-fn main() {
-    let mut tree = PatriciaMerkleTree::<Vec<u8>, Vec<u8>, Keccak256>::new();
+fn main() -> Result<(), Box<dyn Error>> {
+    let n = std::env::args().nth(1).expect("missing number of nodes");
+    let n: usize = n.parse()?;
+    let mut tree = PatriciaMerkleTree::<_, _, Keccak256>::new();
 
-    for x in 1..100 {
-        tree.insert(
-            (0..x).into_iter().collect(),
-            (0..x).into_iter().cycle().take(100).collect(),
-        );
+    let mut rng = thread_rng();
+    let mut key = [0u8; 32];
+    let mut value = [0u8; 32];
+
+    for _ in 0..n {
+        rng.fill_bytes(&mut key);
+        rng.fill_bytes(&mut value);
+        tree.insert(key, value);
     }
 
     let hash = tree.compute_hash();
 
     println!("{hash:x}");
+
+    Ok(())
 }
