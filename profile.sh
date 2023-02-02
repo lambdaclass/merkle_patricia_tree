102c789c123c40f52aed274f2aa176cc5d61d1d9
Edgar Luque <git@edgarluque.com>
2023-02-02 15:35:22 +0100

    improve

--- profile.sh
+++ profile.sh
@@ -9,6 +9,7 @@ rm -f profile-tmp
 mkdir profile-tmp
 
 for i in {1000..1000000..5000}; do
+    echo "Profiling with ${i} nodes."
     echo -en "${i} " >> data.dat
     valgrind --tool=dhat --dhat-out-file=profile-tmp/dhat.out.n-${i} ./target/release-with-debug/examples/calculate-root ${i} 2>&1 \
         | rg -A 4 'Total:' | sed -E 's/==\w+== //g' | rg -o ':\s+([0-9,]+)' -r '$1' | tr -d ',' | tr '\n' ' ' >> data.dat
